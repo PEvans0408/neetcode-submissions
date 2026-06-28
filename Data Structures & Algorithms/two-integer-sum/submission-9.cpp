9990100c03538ced08a97b84139f32926bc2b95a
PEvans0408 <phevans@umass.edu>
2026-06-28 15:06:11 -0400

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for (int idx = 0; idx < nums.size(); idx++) {
+            auto it = hashmap.find(target - nums[idx]);
+            if (it != hashmap.end()) {
+                return {it->second, idx};
+            }
+            else {
+                hashmap.emplace(nums[idx], idx);
+            }   
+        }
+        return {};
+    }
+};
