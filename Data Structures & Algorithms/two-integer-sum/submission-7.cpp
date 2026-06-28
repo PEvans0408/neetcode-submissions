a22c42a81eda74c57818b06a8d6c8a35935895bb
PEvans0408 <phevans@umass.edu>
2026-06-28 14:20:31 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            int pointer1 = nums[i];
+            for (int j = i + 1; j < nums.size(); j++) {
+                int pointer2 = nums[j];
+                if (pointer1 + pointer2 == target) {
+                    vector<int> solution = {i, j};
+                    return solution;
+                }   
+            }
+            continue;
+        }
+    }
+};
